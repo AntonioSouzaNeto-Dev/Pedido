33c42b3f3270023d8b62f1fc94c0e95294c7b506
Antonio Neto <neto.dev.2020@gmail.com>
2023-02-07 22:32:11 -0300

    Criação da função de adicionar e remover items

--- Exe 3/Exe 3/Entities/Order.cs
+++ Exe 3/Exe 3/Entities/Order.cs
@@ -12,21 +12,27 @@ namespace Exe_3.Entities
         public DateTime Moment { get; set; }
         public OrderStatus Status { get; set; }
 
-        public List<OrderItem> orderItems { get; set; } = new List<OrderItem>();
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Client Client { get; set; }
+
         public Order()
         {
         }
-
-        public Order(DateTime moment, OrderStatus status, List<OrderItem> orderItems)
+        public Order(DateTime moment, OrderStatus status, List<OrderItem> items, Client client)
         {
             Moment = moment;
             Status = status;
-            this.orderItems = orderItems;
+            Client = client;
         }
 
-        public void AddItem(Product produtos)
+        public void addItems(OrderItem items)
         {
-           
+            Items.Add(items);
+        }
+        public void removeItems(OrderItem items)
+        {
+            Items.Remove(items);
         }
     }
 }
